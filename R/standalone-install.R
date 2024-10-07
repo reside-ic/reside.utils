5858c63a342f350fa6bddcd56cfca0aca61f6119
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2024-10-07 16:29:31 +0100

    Test installation

--- R/standalone-install.R
+++ R/standalone-install.R
@@ -13,27 +13,35 @@ install_needed <- function(package, need, offer_install = NULL,
 install_needs_list <- function(package, need) {
   desc <- utils::packageDescription(package)
   key <- sprintf("Config/Needs/%s", need)
-  strsplit(desc[[key]], ",\\s+")[[1]]
+  value <- desc[[key]]
+  if (is.null(value)) character() else strsplit(value, ",\\s+")[[1]]
+}
+
+
+install_list_missing_packages <- function(packages) {
+  packages[!vapply(packages, requireNamespace, TRUE, quietly = TRUE)]
 }
 
 
 install_missing <- function(packages,
                             offer_install = NULL,
                             call = parent.frame()) {
-  err <- !vapply(packages, requireNamespace, TRUE, quietly = TRUE)
-  if (!any(err)) {
+  msg <- install_list_missing_packages(packages)
+  if (length(msg) == 0) {
     return()
   }
 
-  msg <- packages[err]
   message <- "Package{?s} missing for this functionality: {.pkg {msg}}"
 
   if (install_offer_install(offer_install)) {
     cli::cli_alert_warning(message)
     ans <- readline("Try installing these packages? [y/N] ")
     if (tolower(ans) == "y") {
-      utils::install.packages(msg)
-      install_missing(msg, FALSE, call)
+      msg <- install_packages(msg)
+      if (length(msg) > 0) {
+        cli::cli_abort(
+          "Failed to install package{?s}: {.pkg {msg}}")
+      }
       cli::cli_alert_success("Installation successful")
       return()
     }
@@ -56,4 +64,10 @@ install_offer_install <- function(offer_install) {
       isTRUE(as.logical(Sys.getenv("NOT_CRAN", "false")))
   }
   offer_install
+}
+
+
+install_packages <- function(packages) {
+  utils::install.packages(packages)
+  install_list_missing_packages(packages)
 }
--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -19,3 +19,85 @@ test_that("validate default for installation", {
   })
   mockery::expect_called(mock_interactive, 4)
 })
+
+
+test_that("can read package requirements", {
+  desc <- structure(list(Package = "foo",
+                         "Config/Needs/this" = "a",
+                         "Config/Needs/that" = "a, b, c"),
+                    class = "packageDescription")
+  mock_pkg_description <- mockery::mock(desc, cycle = TRUE)
+  mockery::stub(install_needs_list, "utils::packageDescription",
+                mock_pkg_description)
+  expect_equal(install_needs_list("foo", "this"), "a")
+  expect_equal(install_needs_list("foo", "that"), c("a", "b", "c"))
+  expect_equal(install_needs_list("foo", "other"), character())
+})
+
+
+test_that("can install missing packages", {
+  mock_list_missing_packages <- mockery::mock(c("a", "c"), character())
+  mock_readline <- mockery::mock("y")
+  mock_install_packages <- mockery::mock()
+
+  mockery::stub(install_missing, "install_list_missing_packages",
+                mock_list_missing_packages)
+  mockery::stub(install_missing, "install_packages",
+                mock_install_packages)
+  mockery::stub(install_missing, "readline", mock_readline)
+
+  res <- evaluate_promise(install_missing(c("a", "b", "c"), TRUE))
+  expect_null(res$result)
+  expect_length(res$messages, 2)
+  expect_match(res$messages[[1]],
+               "Packages missing for this functionality: a and c")
+  expect_match(res$messages[[2]],
+               "Installation successful")
+
+  mockery::expect_called(mock_readline, 1)
+  mockery::expect_called(mock_install_packages, 1)
+  expect_equal(mockery::mock_args(mock_install_packages)[[1]],
+               list(c("a", "c")))
+})
+
+
+test_that("can error if installation refused", {
+  mock_list_missing_packages <- mockery::mock(c("a", "c"))
+  mock_readline <- mockery::mock("n")
+  mock_install_packages <- mockery::mock()
+
+  mockery::stub(install_missing, "install_list_missing_packages",
+                mock_list_missing_packages)
+  mockery::stub(install_missing, "install_packages",
+                mock_install_packages)
+  mockery::stub(install_missing, "readline", mock_readline)
+
+  expect_error(
+    suppressMessages(install_missing(c("a", "b", "c"), TRUE)),
+    "Packages missing for this functionality: a and c")
+
+  mockery::expect_called(mock_readline, 1)
+  mockery::expect_called(mock_install_packages, 0)
+})
+
+
+test_that("can error if installation fails", {
+  mock_list_missing_packages <- mockery::mock(c("a", "c"))
+  mock_readline <- mockery::mock("y")
+  mock_install_packages <- mockery::mock("c")
+
+  mockery::stub(install_missing, "install_list_missing_packages",
+                mock_list_missing_packages)
+  mockery::stub(install_missing, "install_packages",
+                mock_install_packages)
+  mockery::stub(install_missing, "readline", mock_readline)
+
+  expect_error(
+    suppressMessages(install_missing(c("a", "b", "c"), TRUE)),
+    "Failed to install package")
+
+  mockery::expect_called(mock_readline, 1)
+  mockery::expect_called(mock_install_packages, 1)
+  expect_equal(mockery::mock_args(mock_install_packages)[[1]],
+               list(c("a", "c")))
+})
